24563b83b79bc1c5c1717c0ec96b4a8fe24112c1
poiioq <101961422+poiioq@users.noreply.github.com>
2023-06-15 22:08:03 -0400

    Add files via upload

--- ProductManager-bian.java
+++ ProductManager-bian.java
@@ -7,8 +7,8 @@ public class ProductManager {
 	
 	private List<Product> products=new ArrayList<>();
 	
+	//WiteFile
 	public void WiteFile(List<Product> products) throws IOException,ClassNotFoundException {
-		
 		try(ObjectOutputStream output= new ObjectOutputStream(new FileOutputStream("products.dat"))){
 			for (Product product:products) {
 				output.writeObject(product);
@@ -17,8 +17,8 @@ public class ProductManager {
 		
 	}
 	
+	//ReadFile
 	public List<Product> ReadFile() throws IOException, ClassNotFoundException {
-		
 		List<Product> ReadProducts=new ArrayList<>();
 		try(ObjectInputStream input= new ObjectInputStream(new FileInputStream("products.dat"))){
 			while(true) {
@@ -29,15 +29,36 @@ public class ProductManager {
 			}
 		}
 		
-		for (Product product : ReadProducts) {
-            System.out.println(product);
-        }
 		return ReadProducts;
 	}
 	
 	public void addProduct(Product prodcuct) throws ClassNotFoundException, IOException {
 		products.add(prodcuct);
 		WiteFile(products);
+	}
+	
+	//PrintAll
+	public void PrintList(List<Product> products) {
+		
+		for (Product product:products) {
+			System.out.println(product);
+		}
+		
+	}
+
+	// DisplayByRange
+	public List<Product> DisplayByRange(double priceFrom,double priceTo) throws ClassNotFoundException, IOException {
+		List<Product> priceRangeList=ReadFile();
+		
+		for (int i=0;i<priceRangeList.size();i++) {
+			Product product=priceRangeList.get(i);
+			if ((product.getUnitPrice()< priceFrom) || (product.getUnitPrice()> priceTo)){
+				priceRangeList.remove(i);
+				i--;
+			}
+		}
+		
+		return priceRangeList;
 	}
 
 	public static void main(String[] args) {
