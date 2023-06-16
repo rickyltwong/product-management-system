a96bfd649275f66e2b2504bcf722a352ccf7d0d6
poiioq <101961422+poiioq@users.noreply.github.com>
2023-06-15 22:08:30 -0400

    Add files via upload

--- Product-bian.java
+++ Product-bian.java
@@ -10,6 +10,46 @@ public class Product implements Serializable{
 	private int quantity;
 	private double unitPrice;
 	
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
 	public Product(String name,String description,int quantity,double unitPrice) {
 		this.ID=initialID++;
 		this.name=name;
