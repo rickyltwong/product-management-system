9987c9d29f4d801e00fcbd474192bf64fe4a609b
emma418 <105550943+emma418@users.noreply.github.com>
2023-06-17 14:36:23 -0400

    Add files via upload

--- AddOrUpdate.java
+++ AddOrUpdate.java
@@ -1,10 +1,11 @@
-package assignment2_ProductManagementSystem;
+package ProductManagementSystem;
 
 import javax.swing.*;
+
+import java.awt.Color;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,6 +25,8 @@ public class AddOrUpdate {
 
     private static final int NAME_MAX_LENGTH = 20;
     private static final int DESCRIPTION_MAX_LENGTH = 100;
+    private static int currentProduct = 0;
+    Product product;
 
     public AddOrUpdate() {
         createUI();
@@ -47,6 +50,8 @@ public class AddOrUpdate {
         txtDescription.setLineWrap(true);
         JTextField txtQuantity = new JTextField();
         JTextField txtPrice = new JTextField();
+        
+        JLabel prompt = new JLabel();
 
         labelsMap = new HashMap<>();
         labelsMap.put("Product ID", lblProductId);
@@ -54,6 +59,7 @@ public class AddOrUpdate {
         labelsMap.put("Description", lblDescription);
         labelsMap.put("Quantity in hand", lblQuantity);
         labelsMap.put("Unit Price", lblPrice);
+        labelsMap.put("", prompt);
 
         textFieldsMap = new HashMap<>();
         textFieldsMap.put("Product ID", txtProductId);
@@ -80,8 +86,10 @@ public class AddOrUpdate {
         lblProductId.setBounds(30, 20, 100, 20);
         lblName.setBounds(30, 60, 100, 20);
         lblDescription.setBounds(30, 100, 100, 20);
-        lblQuantity.setBounds(330, 100, 100, 20);
+        lblQuantity.setBounds(330, 100, 120, 20);
         lblPrice.setBounds(330, 140, 100, 20);
+        prompt.setBounds(300, 200, 150, 20);
+        prompt.setForeground(Color.RED);
 
         txtProductId.setBounds(160, 20, 120, 20);
         txtName.setBounds(160, 60, 200, 20);
@@ -125,7 +133,7 @@ public class AddOrUpdate {
 
     private void attachEventListeners() {
 
-        // Add key listener to limit the length of name to 20 characters
+        // Add key listener to limit the length of name to 10 characters
         JTextField tfName = textFieldsMap.get("Name");
         tfName.addKeyListener(new KeyAdapter() {
             public void keyTyped(KeyEvent e) {
@@ -144,7 +152,7 @@ public class AddOrUpdate {
 
 
         /*
-         Action listeners for main functions:
+         Other action listeners:
 
          Add - should add the information of new product to the file, product ID should be unique, quantity in hand and unit price should be a number and above 0 and name is required.
 
@@ -170,7 +178,7 @@ public class AddOrUpdate {
             // Check if there is duplicate id (in-place search)
             int pos = -1;
             try {
-                pos = ProductFileHandler.getProductBytePosition(productId);
+                pos = FileHelper.getProductBytePosition(productId);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -183,91 +191,144 @@ public class AddOrUpdate {
             // Write the product into file
             Product product = new Product(productId, name, description, quantity, price);
             try {
-                ProductFileHandler.addProduct(product);
+                FileHelper.addProduct(product);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
 
             // Print out All products (for testing)
             try {
-                ProductFileHandler.printAllProducts();
+                FileHelper.printAllProducts();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
 
         });
+   
+        
 
         buttonsMap.get("Update").addActionListener(evt -> {
 
-            int productId = Integer.parseInt(textFieldsMap.get("Product ID").getText());
-            String name = textFieldsMap.get("Name").getText();
-            String description = txtDescription.getText();
-            int quantity = Integer.parseInt(textFieldsMap.get("Quantity in hand").getText());
-            double price = Double.parseDouble(textFieldsMap.get("Unit Price").getText());
-
-            try {
-                ArrayList<Product> products = ProductFileHandler.getProductListFromFile();
-                Product productToUpdate = findProductInList(products, productId);
-
-                if (productToUpdate == null) {
-                    JOptionPane.showMessageDialog(frame, "Product ID not found");
-                    return;
-                }
-
-                // Update the product
-                productToUpdate.setName(name);
-                productToUpdate.setDescription(description);
-                productToUpdate.setQuantity(quantity);
-                productToUpdate.setUnitPrice(price);
-
-                DataOutputStream dos = ProductFileHandler.getDataOutputStream();
-                for (Product product : products) {
-                    ProductFileHandler.addProduct(product);
-                }
-
-                // Close the data output stream
-                dos.close();
-
-                // Print out All products (for testing)
-                ProductFileHandler.printAllProducts();
-
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
         });
 
         buttonsMap.get("First").addActionListener(evt -> {
-
+        	labelsMap.get("").setText("");
+        	try {
+				firstProduct();
+			} catch (IOException e1) {
+				
+				e1.printStackTrace();
+			}
+        	textFieldsMap.get("Product ID").setText(String.valueOf(product.getId()));
+        	textFieldsMap.get("Name").setText(product.getName());
+           	txtDescription.setText(product.getDescription());
+           	textFieldsMap.get("Quantity in hand").setText(String.valueOf(product.getQuantity()));
+           	textFieldsMap.get("Unit Price").setText(String.valueOf(product.getUnitPrice()));
+         
         });
 
         buttonsMap.get("Previous").addActionListener(evt -> {
+        	labelsMap.get("").setText("");
+        	try {
+				previousProduct();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+        	textFieldsMap.get("Product ID").setText(String.valueOf(product.getId()));
+        	textFieldsMap.get("Name").setText(product.getName());
+           	txtDescription.setText(product.getDescription());
+           	textFieldsMap.get("Quantity in hand").setText(String.valueOf(product.getQuantity()));
+           	textFieldsMap.get("Unit Price").setText(String.valueOf(product.getUnitPrice()));       	
 
         });
+        
 
         buttonsMap.get("Next").addActionListener(evt -> {
+        	labelsMap.get("").setText("");
+        	try {
+				nextProduct();
+				if (product != null) {
+					textFieldsMap.get("Product ID").setText(String.valueOf(product.getId()));
+		        	textFieldsMap.get("Name").setText(product.getName());
+		           	txtDescription.setText(product.getDescription());
+		           	textFieldsMap.get("Quantity in hand").setText(String.valueOf(product.getQuantity()));
+		           	textFieldsMap.get("Unit Price").setText(String.valueOf(product.getUnitPrice()));       
 
+				}
+			} catch (Exception e1) {
+				e1.printStackTrace();
+				labelsMap.get("").setText("An error occurred: " + e1.getMessage());
+			}
+        	
         });
 
         buttonsMap.get("Last").addActionListener(evt -> {
+        	labelsMap.get("").setText("");
+        	try {
+				lastProduct();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+        	textFieldsMap.get("Product ID").setText(String.valueOf(product.getId()));
+        	textFieldsMap.get("Name").setText(product.getName());
+           	txtDescription.setText(product.getDescription());
+           	textFieldsMap.get("Quantity in hand").setText(String.valueOf(product.getQuantity()));
+           	textFieldsMap.get("Unit Price").setText(String.valueOf(product.getUnitPrice()));       
 
         });
+        
+
+
 
 
     }
-
-
-    public static Product findProductInList(ArrayList<Product> products, int productId) {
-        for (Product product : products) {
-            if (product.getId() == productId) {
-                return product;
-            }
-        }
-        return null;
+  	public void firstProduct() throws IOException {
+        currentProduct = 0;
+       	product = FileHelper.readProduct(currentProduct);   	
     }
+  	
+  	public void previousProduct() throws IOException   {
+    	if (currentProduct > 0) {
+    		currentProduct--;
+    		product = FileHelper.readProduct(currentProduct);
+    		
+    	}
+    	else {
+    		product = FileHelper.readProduct(currentProduct);
+    		labelsMap.get("").setText("This is the first record");
+    	}
+    	
+    }
+  	 public void nextProduct() throws Exception  {
+     	if (currentProduct < FileHelper.getProductsCount() - 1) {
+     		currentProduct++;
+     		product = FileHelper.readProduct(currentProduct); 
+     		if (product == null || product.getName().isEmpty()) {
+     			throw new Exception("Product not found");
+     		}
+     	}
+     	else {
+     		labelsMap.get("").setText("This the last record");
+     		product = FileHelper.readProduct(currentProduct);
+     		if (product == null || product.getName().isEmpty()) {
+     			throw new Exception("Product not found");
+     		}
+     	}	
+     		
+     }
+  	  public void lastProduct() throws IOException {
+      	currentProduct = FileHelper.getProductsCount() - 1;
+      	product = FileHelper.readProduct(currentProduct); 
+
+      }
 
     // Main method for testing
-//    public static void main(String[] args) throws FileNotFoundException {
-//        AddOrUpdate addOrUpdate = new AddOrUpdate();
-//    }
-}
+    public static void main(String[] args) throws IOException {
+        AddOrUpdate addOrUpdate = new AddOrUpdate();
+   
+		
 
+    }
+}
